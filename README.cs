6f764d275b83fdb801ad610bd657ec49bb34f36f
GameFilipp <maratstriker2012@gmail.com>
2015-02-07 18:53:11 +0300

    Create README.cs

---
+++ README.cs
@@ -0,0 +1,9 @@
+# Kyky
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a < b) 
+            {
+                Console.WriteLine(" b ");
+                Console.ReadLine();
+            }
